e55b5b1db8ae2cb4ae328c8a185082458607cfbd
danyshio <muqhreezdanysh@gmail.com>
2024-09-28 00:20:56 +0800

    method2

--- Q3.py
+++ Q3.py
@@ -10,3 +10,13 @@ else:
     print(f"Total price = RM{total}")
 
 
+#method2
+drink1 = float(input("Price of drink #1: "))
+drink2 = float(input("Price of drink #2: "))
+
+if drink1 > drink2:
+    total = drink2*0.5 + drink1
+else:
+    total = drink1*0.5 + drink2
+
+print(f"Total price = {total}")

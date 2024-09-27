815e5e0e9cd2626ed5a2762cfd8275660569ff4c
danyshio <muqhreezdanysh@gmail.com>
2024-09-28 00:05:31 +0800

    q1 lab5

---
+++ lab-5/Q1.py
@@ -0,0 +1,6 @@
+first = int(input("Enter 1st number: "))
+second = int(input("Enter 2nd number: "))
+if first > second:
+    print(f"{first} is the largest.")
+else:
+    print(f"{second} is the largest.")

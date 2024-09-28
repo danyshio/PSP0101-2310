678c68ec14f0d2b171a6900c2f685d7b47b9bfe4
danyshio <muqhreezdanysh@gmail.com>
2024-09-29 01:09:42 +0800

    q7

--- lab-5/Q7(b).py
+++ lab-5/Q7(b).py
@@ -3,3 +3,6 @@ hours = float(input("Enter hours: "))
 if hours <= 40:
     payment = hours * 25
     print(f"Payment = RM{payment}")
+else:
+    payment = (40 * 25) + (50*(hours - 40))
+    print(f"Payment = RM{payment}")

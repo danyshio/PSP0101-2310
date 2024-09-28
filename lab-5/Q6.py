aa7e2e8b0be628cc45b10a2c219460b5aebb15fe
danyshio <muqhreezdanysh@gmail.com>
2024-09-29 01:00:17 +0800

    questoin 7(a)

--- lab-5/Q6.py
+++ lab-5/Q6.py
@@ -21,3 +21,22 @@ elif shape == "C":
     print(f"Area = {area}")
 
 
+#method 2
+
+print("A - Rectangle")
+print("B - Triangle")
+print("C - Circle")
+
+shape = input("Enter shape: ")
+
+if(shape == 'A' or shape == 'B'):
+    height = float(input("Enter height: "))
+    base = float(input("Enter base: "))
+    area = height* base
+    if(shape == 'B'):
+        area = 0.5*height* base
+else:
+    radius = float(input("Enter radius: "))
+    area = math.pi*radius**2
+
+print(f"Area = {area}")
---
+++ lab-5/Q7(a).py
@@ -0,0 +1,5 @@
+hours = float(input("Enter hours: "))
+
+if hours < 40:
+    payment = hours * 25
+    print(f"Payment = RM{payment}")

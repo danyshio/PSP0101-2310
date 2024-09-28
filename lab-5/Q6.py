23dfe7baa2232599ade8d8c91dd65bb93be1a211
danyshio <muqhreezdanysh@gmail.com>
2024-09-29 00:50:32 +0800

    question 6

--- lab-5/Q6.py
+++ lab-5/Q6.py
@@ -3,13 +3,21 @@ import math
 print("A - Rectangle")
 print("B - Triangle")
 print("C - Circle")
-
 shape = input("Enter shape: ")
-if shape == A and shape == B:
+
+if shape == "A":
     base = float(input("Enter height: "))
     height = float(input("Enter base: "))
     area = base * height
     print(f"Area = {area}")
-elif shape == C:
+elif shape == "B":
+    base = float(input("Enter height: "))
+    height = float(input("Enter base: "))
+    area = base * height * 0.5
+    print(f"Area = {area}")
+elif shape == "C":
     radius = float(input("Enter radius: "))
-    area = radius * 2
+    area = math.pi * radius ** 2
+    print(f"Area = {area}")
+
+

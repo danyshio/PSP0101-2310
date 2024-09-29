3f4f0140cf2c047ccc2f78e539792afc627e0a72
danyshio <muqhreezdanysh@gmail.com>
2024-09-30 00:05:34 +0800

    test

--- lab-6/Q2.py
+++ lab-6/Q2.py
@@ -7,3 +7,15 @@ while i <=  N:
         if i != N:
             print(", ", end="")
     i += 1
+
+
+#method 2
+
+N = int(input("Enter N: "))
+
+for i in range(N):
+    if (i+1)%2 == 0:
+        print(i+1, end="")
+        if i != N:
+            print(", ", end="")
+    i+=1
